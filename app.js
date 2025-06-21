50f3815f569ebb52cf53c32aa723e6aa2d030ace
Samarth Saxena <163576924+samarth10000@users.noreply.github.com>
2025-06-21 23:08:58 +0530

    lecture -3

--- app.js
+++ app.js
@@ -1,39 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React.createElement => at the end of the day it is a object but when we render this element on to the dom then it becomes a html element
-
-//JSX- Babel transpiles it to React.createElement => ReactElement-js Object => HTMLElement(render)
-
-// this is the way how you will create element in core react
-// const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
-// console.log(heading);
-
-//jsx - HTML like syntax it is not html
-// this is the way how you can create element using jsx
-
-const heading = (
+const Title = () => (
   <h1 className="head" tabIndex="5">
     my name is samarth
   </h1>
 );
-// console.log(heading);
 
+// what is component composition
+// Ans - to use a component inside a another component
 const HeadingComponent = () => (
   <div id="container">
+    <Title />
+    <ThreeDivs />
+    {ThreeDivs()}
+    <ThreeDivs></ThreeDivs>
     <h1 className="heading12">Namaste REACT Functional Component</h1>
   </div>
 );
 
-// the upper and lower code is totally same
+// Extra Notes
 
-// const HeadingComponent2 = () => <h1>Namaste to short syntax</h1>;
+// ✅ When this works:
+
+// The component is a pure function and has no hooks.
+
+// ⚠️ When this is NOT recommended:
+
+// If the component uses Hooks like useState, useEffect, etc.
+
+// React will not treat it as a component — it won't manage its lifecycle properly.
+
+const ThreeDivs = () => {
+  return (
+    <>
+      <div id="fillthem">
+        <div>Hello 1</div>
+        <br />
+        <div>Hello 2</div>
+        <br />
+        <div>Hello 3</div>
+        <br />
+        <div>Hello 4</div>
+        <br />
+      </div>
+    </>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <HeadingComponent />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);
-
-// what do you if heading can be rendered like this , it will go same with the functional components no not at all functional components can not be rendered by render method like this
-
-// root.render(HeadingComponent); // WRONG❌
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
