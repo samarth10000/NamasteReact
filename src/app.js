1c7c3a32cd718baf13a7cc792db75a8cb0557f80
Samarth Saxena <163576924+samarth10000@users.noreply.github.com>
2025-07-03 22:07:52 +0530

    lecture 6

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header.js";
 import Body from "./components/Body.js";
+import COUNT from "./components/Count.js";
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
       <Body />
+      <COUNT />
     </div>
   );
 };
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,43 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RestrauntCard from "./RestrauntCard";
-// import resobj from "../../utils/mockData";
+import resobj from "../../utils/mockData";
 
 const Body = () => {
-  const [listofrestraunts, setlistofrestraunt] = useState([
-    {
-      info: {
-        id: "248400",
-        name: "Cafe Knight Riders",
-        cloudinaryImageId: "bzkakkmq4sysnss5hfdg",
-        locality: "Dodhpur",
-        areaName: "Abad Market",
-        costForTwo: "₹400 for two",
-        cuisines: ["American", "Fast Food", "Pizzas", "Burgers"],
-        avgRating: 4,
-        parentId: "14948",
-        avgRatingString: "4.0",
-        totalRatingsString: "2.6K+",
-        deliveryTime: 47,
-      },
-    },
-    {
-      info: {
-        id: "248401",
-        name: "Sai Dhaba",
-        cloudinaryImageId: "bzkakkmq4sysnss5hfdg",
-        locality: "Dodhpur",
-        areaName: "Abad Market",
-        costForTwo: "₹400 for two",
-        cuisines: ["Indian", "Thali", "Roti Sabzi"],
-        avgRating: 4.5,
-        parentId: "14949",
-        avgRatingString: "4.5",
-        totalRatingsString: "3.1K+",
-        deliveryTime: 32,
-      },
-    },
-  ]);
+  //we can see this state as a array destructuring , this syntax and second one works the same , here just showing what is shappening inside it
+  const arr = useState(resobj);
 
+  const listofrestraunts = arr[0];
+  const setlistofrestraunt = arr[1];
+
+  // super powerful state local state variable
+  // const [listofrestraunts, setlistofrestraunt] = useState(resobj);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.9615398&lng=79.2961468&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    setlistofrestraunt(
+      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
   return (
     <div className="body">
       <div className="filter">
