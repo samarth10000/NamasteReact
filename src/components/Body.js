b96ad1b1971fc915ff16fe84dc608b327399f500
Samarth Saxena <163576924+samarth10000@users.noreply.github.com>
2025-07-06 18:54:58 +0530

    Lecture 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import RestrauntCard from "./RestrauntCard";
 import resobj from "../../utils/mockData";
+import ShimmerUI from "./ShimmerUI";
 
 const Body = () => {
   //we can see this state as a array destructuring , this syntax and second one works the same , here just showing what is shappening inside it
@@ -11,7 +12,6 @@ const Body = () => {
 
   const [searchtext, setsearchtext] = useState("");
   // whenever state variables update , react triggers a reconciliation cycle(re-renders the component)
-  console.log("Body rendered");
 
   // super powerful state local state variable
   // const [listofrestraunts, setlistofrestraunt] = useState(resobj);
@@ -22,13 +22,17 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139&lng=77.2090&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     console.log(json);
 
-    // setlistofrestraunt(json.data.cards[4].card.card.brands);
+    // setlistofrestraunt(json.data.cards[2].data.data.cards);
   };
+
+  if (listofrestraunts.length === 0) {
+    return <ShimmerUI />;
+  }
   return (
     <div className="body">
       <div className="filter">
---
+++ src/components/ShimmerUI.js
@@ -0,0 +1,30 @@
+import Header from "./Header";
+
+const ShimmerUI = () => {
+  return (
+    <div>
+      <Header />
+
+      <div className="ShimmerCards">
+        <div>Restraunt</div>
+      </div>
+      <div className="ShimmerCards">
+        <div>Restraunt</div>
+      </div>
+      <div className="ShimmerCards">
+        <div>Restraunt</div>
+      </div>
+      <div className="ShimmerCards">
+        <div>Restraunt</div>
+      </div>
+      <div className="ShimmerCards">
+        <div>Restraunt</div>
+      </div>
+      <div className="ShimmerCards">
+        <div>Restraunt</div>
+      </div>
+    </div>
+  );
+};
+
+export default ShimmerUI;
